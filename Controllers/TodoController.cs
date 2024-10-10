d5421ff32cfe7d94704b48f756b83db03d562c6c
Katelli <aska082024@gmail.com>
2024-10-10 13:01:22 +0200

    Created todo list for TodoController

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+/*
+[ApiController]
+[Route("todo")]
+
+public class TodoController : ControllerBase
+
+    Create
+    [HttpPost]
+    public Method?
+    {
+        Code Logic
+    }
+
+    Read
+    [HttpGet]
+    public Method?
+    {
+        Code Logic
+    }
+
+    Read (get by id)
+    [HttpGet("{id}")]
+    public Method?
+    {
+        Code Logic
+    }
+
+    Update
+    [HttpPut("{id}")]
+    public Method?
+    {
+        Code Logic
+    }
+
+    Delete
+    [HttpDelete("{id}")]
+    public Method?
+    {
+        Code Logic
+    }
+*/
