478114e6b7d9b810178e6285c771de5a29a52537
bke82024 <bke82024@gmail.com>
2024-10-14 14:36:40 +0200

    js

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,10 +1,40 @@
-/* const inputField = document.getElementById('input')
-const labelContainer = document.labelContainer('label')
+let toDoes = [];
+let inputField = document.querySelector("input");
+const addButton = document.querySelector("#add");
 
+const container = document.querySelector("#container");
+const deleteButton = document.querySelector("delete");
 
+function createTodo(title) {
+  const newTodo = {
+    title: title,
+    isComplete: false,
+  };
 
-const node.addEventListener("keyup"), ({key}) => {
-    if (key === "Enter") { Event createElement
-        }
+  const newList = document.createElement("li");
+  const label = document.createElement("label");
+  const progress = document.createElement("p");
+  const completeBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  label.textContent = title;
+
+  newList.appendChild(label);
+  newList.appendChild(progress);
+  newList.appendChild(completeBtn);
+  newList.appendChild(deleteBtn);
+  deleteBtn.id = "delete";
+
+  toDoes.push(newTodo);
+  return newList;
 }
-        */
+
+function handleAddTodo() {
+  inputValue = inputField.value;
+
+  const newList = createTodo(inputValue);
+
+  container.appendChild(newList);
+}
+
+addButton.addEventListener("click", handleAddTodo);
