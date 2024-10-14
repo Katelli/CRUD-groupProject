d4898ee1ea91a9205fc4dfe0ca1e25c4e5be1fe4
bke82024 <bke82024@gmail.com>
2024-10-14 13:13:02 +0200

    task label styling

---
+++ wwwroot/main.js
@@ -0,0 +1,10 @@
+/* const inputField = document.getElementById('input')
+const labelContainer = document.labelContainer('label')
+
+
+
+const node.addEventListener("keyup"), ({key}) => {
+    if (key === "Enter") { Event createElement
+        }
+}
+        */
